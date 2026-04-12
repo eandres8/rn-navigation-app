1c91f1a4b2b4197f773cc68d28e6aeb3ac293593
Andres Vega <elb.andres8@gmail.com>
2026-04-12 18:02:12 -0500

    feat: config native tailwind

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,13 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-work-black text-primary">
+        Welcome to Nativewind!
+      </Text>
+      <Link href="/products">Productos</Link>
     </View>
   );
 };
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./App.tsx", "./src/components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      fontFamily: {
+        'work-black': ['WorkSans-Black', 'sans-serif'],
+        'work-light': ['WorkSans-Light', 'sans-serif'],
+        'work-medium': ['WorkSans-Medium', 'sans-serif'],
+      },
+      colors: {
+        primary: '#49129C',
+        secondary: {
+          DEFAULT: '#B40086',
+          100: '#C51297',
+          200: '#831266',
+        },
+        tertiary: '#EF2967'
+      }
+    },
+  },
+  plugins: [],
+}
+
